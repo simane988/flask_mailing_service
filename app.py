77d5bf121b187b4ab423dfcbee185d1d4bd63839
simane988 <simane988@gmail.com>
2022-12-06 11:12:26 +0300

    Just some new stuff

--- app.py
+++ app.py
@@ -22,5 +22,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/auth')
+def auth():
+    return render_template('auth.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
